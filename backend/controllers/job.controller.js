2b9e19fceb5fb93ecbd92810812ef620ad21da93
karun217 <karunpoddar0@gmail.com>
2025-05-17 13:30:46 +0530

    add job controller

---
+++ backend/controllers/job.controller.js
@@ -0,0 +1,44 @@
+import { Job } from "../models/job.model.js";
+
+// admin post krega job
+export const postJob = async (req, res) => {
+    try {
+        const { title, description, requirements, salary, location, jobType, experience, position, companyId } = req.body;
+        const userId = req.id;
+
+        if (!title || !description || !requirements || !salary || !location || !jobType || !experience || !position || !companyId) {
+            return res.status(400).json({
+                message: "Something is missing.",
+                success: false
+            })
+        };
+
+        const parsedSalary = Number(salary);
+        if (isNaN(parsedSalary)) {
+            return res.status(400).json({
+                message: "Salary must be a valid number.",
+                success: false
+            });
+        }
+
+        const job = await Job.create({
+            title,
+            description,
+            requirements: requirements.split(","),
+            salary: Number(salary),
+            location,
+            jobType,
+            experienceLevel: experience,
+            position,
+            company: companyId,
+            created_by: userId
+        });
+        return res.status(201).json({
+            message: "New job created successfully.",
+            job,
+            success: true
+        });
+    } catch (error) {
+        console.log(error);
+    }
+}
