b9aa9ac43dc0cb225291bcf1bd57fefdf4653f7b
karun217 <karunpoddar0@gmail.com>
2025-05-22 22:02:11 +0530

    set job component

---
+++ frontend/src/components/Job.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Button } from './ui/button'
+import { Bookmark } from 'lucide-react'
+import { Avatar, AvatarImage } from './ui/avatar'
+import { Badge } from './ui/badge'
+import { useNavigate } from 'react-router-dom'
+
+const Job = ({ job }) => {
+    const navigate = useNavigate();
+
+    const daysAgoFunction = (mongodbTime) => {
+        const createdAt = new Date(mongodbTime);
+        const currentTime = new Date();
+        const timeDifference = currentTime - createdAt;
+        return Math.floor(timeDifference / (1000 * 60 * 60 * 24));
+    }
