68e1a847a3a577496d2d0df64e216519b374d997
karun217 <karunpoddar0@gmail.com>
2025-07-03 17:12:06 +0530

    add job description

--- frontend/src/components/JobDescription.jsx
+++ frontend/src/components/JobDescription.jsx
@@ -121,3 +121,60 @@ const JobDescription = () => {
           </Button>
         </div>
       </div>
+
+       <h1 className='border-b-2 border-b-gray-300 font-medium py-4'>
+        Job Description
+      </h1>
+
+      <div className='my-4'>
+        <h1 className='font-bold my-1'>
+          Role:{' '}
+          <span className='pl-4 font-normal text-gray-800'>
+            {singleJob?.title}
+          </span>
+        </h1>
+        <h1 className='font-bold my-1'>
+          Location:{' '}
+          <span className='pl-4 font-normal text-gray-800'>
+            {singleJob?.location}
+          </span>
+        </h1>
+        <h1 className='font-bold my-1'>
+          Description:{' '}
+          <span className='pl-4 font-normal text-gray-800'>
+            {singleJob?.description}
+          </span>
+        </h1>
+        <h1 className='font-bold my-1'>
+          Experience:{' '}
+          <span className='pl-4 font-normal text-gray-800'>
+            {singleJob?.experience ? `${singleJob?.experience} yrs` : 'N/A'}
+          </span>
+        </h1>
+        <h1 className='font-bold my-1'>
+          Salary:{' '}
+          <span className='pl-4 font-normal text-gray-800'>
+            {singleJob?.salary
+              ? `${(singleJob.salary / 100000).toFixed(1)} LPA`
+              : 'N/A'}
+          </span>
+        </h1>
+
+        <h1 className='font-bold my-1'>
+          Total Applicants:{' '}
+          <span className='pl-4 font-normal text-gray-800'>
+            {singleJob?.applications?.length}
+          </span>
+        </h1>
+        <h1 className='font-bold my-1'>
+          Posted Date:{' '}
+          <span className='pl-4 font-normal text-gray-800'>
+            {singleJob?.createdAt?.split('T')[0]}
+          </span>
+        </h1>
+      </div>
+    </div>
+  )
+}
+
+export default JobDescription
