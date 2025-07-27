6e485b5cfda97b034876bd2ebc9bc202b7dc316c
utkarshup32 <utkarshdwivedi1879@gmail.com>
2025-07-27 11:53:00 +0530

    added routes

--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -1,7 +1,14 @@
 import express from "express";
 import { login, logout, register, updateProfile } from "../controllers/user.controller.js";
 import isAuthenticated from "../middlewares/isAuthenticated.js";
-import { singleUpload } from "../middlewares/mutler.js";
+import { singleUpload } from "../middlewares/multer.js";
+
+
+import {
+  saveJobForLater,
+  getSavedJobs,
+  removeSavedJob
+} from "../controllers/user.controller.js";
  
 const router = express.Router();
 
@@ -10,5 +17,10 @@ router.route("/login").post(login);
 router.route("/logout").get(logout);
 router.route("/profile/update").post(isAuthenticated,singleUpload,updateProfile);
 
-export default router;
+router.post("/bookmark/:jobId", isAuthenticated, saveJobForLater);
+router.get("/bookmarks", isAuthenticated, getSavedJobs);
 
+
+router.delete("/bookmark/:jobId", isAuthenticated, removeSavedJob);
+
+export default router;
